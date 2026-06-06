9c64f807b8ee9b066358fcd92feff822126332b1
xxufnaviin <naviin2373@gmail.com>
2026-06-06 17:59:49 +0800

    fix tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite"; // 1. Add this import
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths(),nitro() // 2. Add the Nitro server plugin here
+  ],
 });
